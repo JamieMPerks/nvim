9702157908a2d4315dc3b444e605b6f2384defee
JamieMPerks <145720018+JamieMPerks@users.noreply.github.com>
2024-11-02 16:53:12 -0400

    Update harpoon.lua
    Add shortcut keys to open in new tab/splits from the quick menu

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -41,5 +41,21 @@ return {
 		vim.keymap.set("n", "<leader>hn", function()
 			harpoon:list():next()
 		end, { desc = "Next item" })
+
+		harpoon:extend({
+			UI_CREATE = function(cx)
+				vim.keymap.set("n", "<C-v>", function()
+					harpoon.ui:select_menu_item({ vsplit = true })
+				end, { buffer = cx.bufnr })
+
+				vim.keymap.set("n", "<C-x>", function()
+					harpoon.ui:select_menu_item({ split = true })
+				end, { buffer = cx.bufnr })
+
+				vim.keymap.set("n", "<C-t>", function()
+					harpoon.ui:select_menu_item({ tabedit = true })
+				end, { buffer = cx.bufnr })
+			end,
+		})
 	end,
 }
